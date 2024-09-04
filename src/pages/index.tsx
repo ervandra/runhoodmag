8659778228df7945370d7427e7c789802e776ae2
Ervandra Halim <ervandra.halim@gmail.com>
2024-09-04 14:56:26 +0700

    update final revision

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,11 +13,11 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between ${inter.className}`}
     >
-      <div className="header bg-black py-10 w-full">
+      <div className="header bg-black py-7 md:py-10 w-full">
         <div className="container mx-auto px-10">
-          <div className="flex justify-center md:justify-between items-center">
+          <div className="flex  items-center">
             <div className="logo">
-              <img src="/images/logo.png" alt="Logo" width="146" />
+              <img src="/images/logo.png" alt="Logo" className="h-3 md:h-5" />
             </div>
           </div>
         </div>
@@ -38,95 +38,95 @@ export default function Home() {
             >
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner1.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner1.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner2.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner2.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner3.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner3.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner4.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner4.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner5.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner5.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner6.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner6.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner7.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner7.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner8.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner8.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner9.jpg" alt="banner" />
+                  <img src="/images/banners/desktop/banner9.jpg" alt="banner" />
                 </div>
               </SwiperSlide>
               <SwiperSlide>
                 <div className="carousel-item">
-                  <img src="/images/banner10.jpg" alt="banner" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="carousel-item">
-                  <img src="/images/banner11.jpg" alt="banner" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="carousel-item">
-                  <img src="/images/banner12.jpg" alt="banner" />
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className="carousel-item">
-                  <img src="/images/banner13.jpg" alt="banner" />
+                  <img
+                    src="/images/banners/desktop/banner10.jpg"
+                    alt="banner"
+                  />
                 </div>
               </SwiperSlide>
             </Swiper>
           </div>
         </div>
-        <div className="main-about py-24 bg-black  px-10">
+        <div className="main-about py-8 md:py-24 bg-black px-5 md:px-10">
           <div className="container mx-auto text-white">
-            <h1 className="mb-10 text-left text-[#E21D03] text-2xl md:text-[32px] leading-snug font-bold">
-            RUNHOOD IS A MEDIA COMMITTED TO FOSTERING THE GROWTH OF SPORTS, WITH A PARTICULAR FOCUS ON PROMOTING RUNNING IN INDONESIA.
+            <h1 className="mb-10 text-left text-[#E21D03] text-base md:text-[32px] leading-snug font-bold">
+              RUNHOOD IS A MEDIA COMMITTED TO FOSTERING THE GROWTH OF SPORTS,
+              WITH A PARTICULAR FOCUS ON PROMOTING RUNNING IN INDONESIA.
             </h1>
-            <p className="md:text-xl mb-4">We are here to inspire as many people as possible through mass sports participation and are committed to support the nation&lsquo;s sporting excellence.</p>
-            <p className="md:text-xl">We believe that sport is the foundation for our nation&lsquo;s well-being, unity, education, and many other positive impacts. </p>
+            <div className="flex justify-end">
+              <div className="w-11/12 md:w-9/12">
+                <p className="text-sm md:text-xl mb-4">
+                  We are here to inspire as many people as possible through mass
+                  sports participation and are committed to support the
+                  nation&lsquo;s sporting excellence.
+                </p>
+                <p className="md:text-xl">
+                  We believe that sport is the foundation for our nation&lsquo;s
+                  well-being, unity, education, and many other positive impacts.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="main-watch pt-24 pb-12 px-10">
+        <div className="main-watch pt-8 md:pt-24 pb-5 md:pb-12">
           <div className="container mx-auto">
-            <div className="flex items-center justify-between mb-16">
+            <div className="flex items-center justify-between mb-5 md:mb-16 px-5 md:px-10">
               <h2 className="text-2xl md:text-[50px] font-[900]">WATCH</h2>
               <a
                 href="https://www.youtube.com/@RunhoodMag"
-                className="uppercase"
+                className="uppercase hidden md:block"
                 rel="noopener noreferrer"
                 target="_blank"
               >
                 All Videos 🡒
               </a>
             </div>
-            <div className="mb-16 grid grid-cols-1 md:grid-cols-12 gap-10">
+            <div className="mb-5 md:mb-16 grid grid-cols-1 md:grid-cols-12 gap-10 px-0 md:px-10">
               <div className="col-span-full md:col-span-7">
                 <div className="watch-item watch-large">
                   <div className="watch-thumbnail mb-5">
@@ -142,26 +142,28 @@ export default function Home() {
                       />
                     </a>
                   </div>
-                  <p className="watch-category mb-4 text-xl">
-                    Running For Dummies
-                  </p>
-                  <h3 className="watch-title mb-4 text-xl md:text-[32px] font-bold uppercase">
-                    <a
-                      href="https://www.youtube.com/watch?v=t1jMNh3UQLM"
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      BISA GAK SIH LARI TANPA SMARTWATCH?
-                    </a>
-                  </h3>
-                  <p className="watch-description text-xl">
-                    Kenapa sih, sekarang semua pelari pake smartwatch? Emang
-                    seperlu itu ya? 🤔 Eits, makanya di video ini Minhood
-                    jelasin riset tentang smartwatch dan apa aja kegunaannya.
-                  </p>
+                  <div className="px-5 md:px-0">
+                    <p className="watch-category mb-2 md:mb-4 text-xs md:text-xl uppercase">
+                      Running For Dummies
+                    </p>
+                    <h3 className="watch-title mb-4 text-2xl md:text-[32px] font-bold uppercase">
+                      <a
+                        href="https://www.youtube.com/watch?v=t1jMNh3UQLM"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        BISA GAK SIH LARI TANPA SMARTWATCH?
+                      </a>
+                    </h3>
+                    <p className="watch-description text-sm md:text-xl leading-tight">
+                      Kenapa sih, sekarang semua pelari pake smartwatch? Emang
+                      seperlu itu ya? 🤔 Eits, makanya di video ini Minhood
+                      jelasin riset tentang smartwatch dan apa aja kegunaannya.
+                    </p>
+                  </div>
                 </div>
               </div>
-              <div className="col-span-full md:col-span-5 flex flex-col gap-5">
+              <div className="col-span-full md:col-span-5 flex flex-col gap-4 md:gap-5 px-5 md:px-0">
                 <div className="watch-item watch-medium grid grid-cols-2 gap-4 items-center">
                   <div className="watch-thumbnail">
                     <a
@@ -176,7 +178,7 @@ export default function Home() {
                       />
                     </a>
                   </div>
-                  <h3 className="watch-title text-lg font-bold leading-tight uppercase">
+                  <h3 className="watch-title text-sm md:text-lg font-bold leading-tight uppercase">
                     <a
                       href="https://www.youtube.com/watch?v=EM8juFYLrRk"
                       rel="noopener noreferrer"
@@ -201,7 +203,7 @@ export default function Home() {
                       />
                     </a>
                   </div>
-                  <h3 className="watch-title text-lg font-bold leading-tight uppercase">
+                  <h3 className="watch-title text-sm md:text-lg font-bold leading-tight uppercase">
                     <a
                       href="https://www.youtube.com/watch?v=XejzOYibuwQ"
                       rel="noopener noreferrer"
@@ -225,7 +227,7 @@ export default function Home() {
                       />
                     </a>
                   </div>
-                  <h3 className="watch-title text-lg font-bold leading-tight uppercase">
+                  <h3 className="watch-title text-sm md:text-lg font-bold leading-tight uppercase">
                     <a
                       href="https://www.youtube.com/watch?v=gA85jHU1sWk"
                       rel="noopener noreferrer"
@@ -250,7 +252,7 @@ export default function Home() {
                       />
                     </a>
                   </div>
-                  <h3 className="watch-title text-lg font-bold leading-tight uppercase">
+                  <h3 className="watch-title text-sm md:text-lg font-bold leading-tight uppercase">
                     <a
                       href="https://www.youtube.com/watch?v=w2qKLTQifPs"
                       rel="noopener noreferrer"
@@ -262,15 +264,25 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            <div className="px-5 md:px-0">
+              <a
+                href="https://www.youtube.com/@RunhoodMag"
+                className="uppercase block md:hidden text-sm"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                All Videos 🡒
+              </a>
+            </div>
           </div>
         </div>
-        <div className="main-latest pb-24 pt-12">
+        <div className="main-latest pb-5 md:pb-24 pt-0 md:pt-12">
           <div className="container mx-auto">
-            <div className="flex items-center justify-between mb-16">
+            <div className="flex items-center justify-between mb-3 md:mb-16 mx-5 md:mx-0 pt-5 border-t md:border-0">
               <h2 className="text-2xl md:text-[50px] font-[900]">LATEST</h2>
               <a
                 href="https://www.instagram.com/runhoodmag/"
-                className="uppercase"
+                className="uppercase hidden md:block"
                 rel="noopener noreferrer"
                 target="_blank"
               >
@@ -375,28 +387,40 @@ export default function Home() {
               </Swiper>
             </div>
           </div>
+          <div className="px-5 md:px-0 pt-5 md:pt-0">
+          <a
+                href="https://www.instagram.com/runhoodmag/"
+                className="uppercase block md:hidden text-sm"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Follow along on Instagram 🡒
+              </a>
+          </div>
         </div>
       </div>
-      <div className="main-contact bg-[#E21D03] py-28 w-full">
-        <div className="container mx-auto px-10">
-          <div className="flex items-center justify-between">
-            <h2 className="text-white font-[900] text-xl md:text-[50px] leading-tight uppercase m-0">
+      <div className="main-contact bg-[#E21D03] py-5 md:py-28 w-full">
+        <div className="container mx-auto px-5 md:px-10">
+          <div className="flex items-start md:items-center md:justify-between flex-col md:flex-row">
+            <h2 className="text-white font-[900] text-xl md:text-[50px] leading-tight uppercase mb-2 md:m-0">
               Connect With Us
             </h2>
             <a
               href="mailto:admin@athleticacompany.com"
               rel="noopener noreferrer"
               target="_blank"
-              className="text-white bg-black py-3 px-6 font-bold"
+              className="text-white bg-black py-3 px-6 font-bold text-sm md:text-base"
             >
               Get in touch
             </a>
           </div>
         </div>
       </div>
-      <div className="main-footer bg-black py-28 w-full">
-        <div className="container mx-auto px-10">
-          <h3 className="text-white text-3xl md:text-[32px] text-center md:text-left leading-tight font-bold">
+      <div className="main-footer bg-black py-14 md:py-28 w-full">
+        <div className="container mx-auto px-5 md:px-10">
+          <div className="grid grid-cols-12 gap-5 items-start">
+            <div className="col-span-7 md:col-span-full">
+          <h3 className="text-white text-2xl md:text-[32px] text-left leading-tight font-bold">
             Discover
             <br />
             The Running
@@ -405,12 +429,44 @@ export default function Home() {
             <br />
             in Indonesia
           </h3>
+          </div>
+          <div className="links flex flex-col gap-2 items-start justify-end order-1 md:order-2 md:hidden">
+              <a
+                href="https://www.youtube.com/@RunhoodMag/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Youtube
+              </a>
+              <a
+                href="https://www.instagram.com/runhoodmag/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://www.tiktok.com/@runhoodmag?lang=en"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Tiktok
+              </a>
+              <a
+                href="https://www.strava.com/clubs/286856"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Strava
+              </a>
+            </div>
+          </div>
 
-          <div className="flex flex-col md:flex-row justify-between items-center mt-20 gap-10">
-            <div className="copyright text-white font-bold order-2 md:order-1">
+          <div className="flex flex-col md:flex-row md:justify-between md:items-center mt-10 md:mt-20 gap-10">
+            <div className="copyright text-white font-bold order-2 md:order-1 w-1/2 md:w-full">
               &copy; 2024 RUNHOOD. All Rights Reserved.
             </div>
-            <div className="links flex gap-4 items-center justify-end order-1 md:order-2">
+            <div className="links gap-4 items-center justify-end order-1 md:order-2 hidden md:flex">
               <a
                 href="https://www.youtube.com/@RunhoodMag/"
                 rel="noopener noreferrer"
